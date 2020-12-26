f4ded67efcdc71258a83738f4d03b1a00d0a7e11
laxedo17 <saracyan78@hotmail.com>
2020-12-26 22:08:24 +0100

    Add project files.

---
+++ Emailer Informe Curso/Modelos/InformeDetalladoMatriculas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emailer_Informe_Curso.Modelos
+{
+    class InformeDetalladoMatriculas
+    {
+        public int IdMatricula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string CodigoCurso { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Emailer Informe Curso/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emailer_Informe_Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
